80995577bfc9672b0a3e7c4f4557c7796b3cad60
TuGG <tugg.solo@gmail.com>
2016-03-05 14:08:21 +0700

    express

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+'use strict';
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 8080));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use('/libs', express.static(__dirname + '/node_modules'));
+app.use('/css', express.static(__dirname + '/src/static/css'));
+app.use('/js', express.static(__dirname + '/src/static/js'));
+app.use('/img', express.static(__dirname + '/src/static/img'));
+app.use('/favicon.ico', express.static(__dirname + '/src/static/img/favicon.ico'));
+app.use('/fonts', express.static(__dirname + '/src/static/fonts'));
+app.use('/resources', express.static(__dirname + '/src/static/resources'));
+
+app.get('/', function (req, res) {
+	res.status(200).render('index.html');
+});
+app.listen(app.get('port'), function () {
+    console.log('App is running on port ' + app.get('port'));
+});
